ee41df935e4e42180bc975ad2bf55cbc4f05405f
Brett Jaynes <brettj@minim.com>
2023-02-13 09:57:22 -0800

    SW-3301 fixing string concat issue in archive script, adding some prints

--- bin/circleci_archivebuild.sh
+++ bin/circleci_archivebuild.sh
@@ -9,12 +9,17 @@ GIT_REVISION="${CIRCLE_SHA1:-$(git rev-parse HEAD)}"
 
 VERSION=$(cat base_version)-$GIT_REVISION
 eval $(grep CONFIG_TARGET_BOARD= openwrt/.config)
+echo "Config target board = $CONFIG_TARGET_BOARD"
 eval $(grep CONFIG_TARGET_SUBTARGET= openwrt/.config)
+echo "Config target subtarget = $CONFIG_TARGET_SUBTARGET"
 eval $(grep CONFIG_ARCH= openwrt/.config)
+echo "Config architecture = $CONFIG_ARCH"
 eval $(grep CONFIG_CPU_TYPE= openwrt/.config)
+echo "Config CPU type = $CONFIG_CPU_TYPE"
 eval $(grep CONFIG_LIBC= openwrt/.config)
-TARGET_BIN_DIR=openwrt/bin/targets/$CONFIG_TARGET_BOARD/$CONFIG_TARGET_SUBTARGET
-TARGET_ROOT_DIR=openwrt/build_dir/target-$CONFIG_ARCH_$CONFIG_CPU_TYPE_$CONFIG_LIBC*/root-$CONFIG_TARGET_BOARD
+echo "Config libc = $CONFIG_LIBC"
+TARGET_BIN_DIR=openwrt/bin/targets/${CONFIG_TARGET_BOARD}/${CONFIG_TARGET_SUBTARGET}
+TARGET_ROOT_DIR=openwrt/build_dir/target-${CONFIG_ARCH}_${CONFIG_CPU_TYPE}_${CONFIG_LIBC}*/root-${CONFIG_TARGET_BOARD}
 cp $TARGET_BIN_DIR/*factory* $DEVICE/$DEVICE-factory-$VERSION.bin || cp $TARGET_BIN_DIR/*sysupgrade* $DEVICE/$DEVICE-factory-$VERSION.bin
 cp $TARGET_BIN_DIR/*sysupgrade* $DEVICE/$DEVICE-sysupgrade-$VERSION.bin
 cp $TARGET_BIN_DIR/*.manifest $DEVICE/$DEVICE-$VERSION.manifest
